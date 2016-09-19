aba33aa4f738d30b81608404bdaf975a6008069d
nar142857 <764162156@qq.com>
2016-09-19 18:14:57 +0800

    work 20160919

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+//var express = require('express');
+//var router = express.Router();
+//
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//module.exports = router;
 
-module.exports = router;
+var routers = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', {title: 'Express'}); //参数一：模板名称即 views 目录下的模板文件名，参数二：数据
+    });
+    app.get('/reg', function (req, res) {
+        res.render('reg', {title: '注册'});
+    });
+    app.post('/reg', function (req, res) {
+    });
+    app.get('/login', function (req, res) {
+        res.render('login', {title: '登录'});
+    });
+    app.post('/login', function (req, res) {
+    });
+    app.get('/post', function (req, res) {
+        res.render('post', {title: '发表'});
+    });
+    app.post('/post', function (req, res) {
+    });
+    app.get('/logout', function (req, res) {
+    });
+};
+
+module.exports = routers;
