610f5fc41c9370163037b47acdb32113c7f1cfc6
nar142857 <764162156@qq.com>
2016-09-19 18:09:42 +0800

    work

---
+++ settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/9/19 0019.
+ */
+module.exports = {
+    cookieSecret: 'myblog',  //用于cookie加密
+    db: 'blog',              //数据库名称
+    host: 'localhost',
+    port: 27017
+};
